c2a7a279edac3cd9478796fb403587e826eaad2b
poprahul <poprahul@amazon.com>
2021-03-04 23:32:39 -0500

    First commit

---
+++ src/aws-exports.js
@@ -0,0 +1,8 @@
+const awsmobile = {
+    "aws_appsync_graphqlEndpoint": "https://ahgbuvaecjeq7pbngssbyucxqa.appsync-api.us-east-1.amazonaws.com/graphql",
+    "aws_appsync_region": "us-east-1",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-wci6t4hxqncbdeokmrbcj6h4gy",
+};
+
+export default awsmobile;
